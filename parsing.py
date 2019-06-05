f310ab8b61c6e341047eb2f611e67f0086250653
daniel.brown1 <daniel.brown1@mail.huji.ac.il>
2019-06-05 22:42:07 +0300

    done parsing legnth, longest & shortest word, splat tweet

--- parsing.py
+++ parsing.py
@@ -17,8 +17,12 @@ names = ["Donald Trump",
          "Jimmy kimmel",
          "Arnold schwarzenegger"]
 
+_tweets = []
+
 
 def get_tweets(path):
+    path = "data/" + path
+
     return pd.read_csv(path)
 
 
@@ -32,12 +36,53 @@ def extract_tags():
 
 def extract_len(tweet):
     tweet = tweet.split(' ')
-    number_of_words = len(tweet)
-    longest_word_length = min(tweet, key = len)
-    shortest_word_length = max(tweet, key = len)
-    return tweet, number_of_words, longest_word_length, shortest_word_length
+    return tweet
 
 
-def main():
-    for path in pathes:
-        tweets = get_tweets(path)
+def split_tweet(tweets_list):
+    empty = []
+    for tweet in tweets_list:
+        tweet = tweet.replace("[", "")
+        tweet = tweet.replace("]", "")
+        tweet = tweet.replace("\\", "")
+        tweet = tweet.replace(",", "")
+        tweet = tweet[1:-1]
+        empty.append(tweet.split(' '))
+    return empty
+
+
+def get_tweet_len(tweets_list):
+    empty = []
+    for tweet in tweets_list:
+        empty.append(len(tweet))
+    return empty
+
+
+def length(str):
+    return len(str)
+
+
+def get_longest_word(tweets_list):
+    empty = []
+    for tweet in tweets_list:
+        longest = max(tweet, key = length)
+        empty.append(len(longest))
+    return empty
+
+
+def get_shortest_word(tweets_list):
+    empty = []
+    for tweet in tweets_list:
+        shortest = min(tweet, key = length)
+        empty.append(len(shortest))
+    return empty
+
+
+if __name__ == "__main__":
+    frames = [get_tweets(f) for f in pathes]
+    all_tweets = pd.concat(frames)
+    all_tweets_np = all_tweets.to_numpy()
+    all_tweets['broken_to_words'] = split_tweet(all_tweets_np[:, 1])
+    all_tweets['number_of_words'] = get_tweet_len(all_tweets_np[:, 1])
+    all_tweets['longest_word_length'] = get_longest_word(all_tweets['broken_to_words'])
+    all_tweets['shortest_word_length'] = get_shortest_word(all_tweets['broken_to_words'])
