eacf4edc86facdd859698435e5fbda328b7a6422
daniel.brown1 <daniel.brown1@mail.huji.ac.il>
2019-06-05 20:24:27 +0300

    some parsing functions, data included

--- parsing.py
+++ parsing.py
@@ -1,5 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+
+pathes = ["ConanOBrien_tweets.csv", "cristiano_tweets.csv", "donaldTrump_tweets.csv", "ellenShow_tweets.csv",
+          "jimmykimmel_tweets.csv", "joeBiden_tweets.csv", "KimKardashian_tweets.csv", "labronJames_tweets.csv",
+          "ladygaga_tweets.csv", "Schwarzenegger_tweets.csv"]
+
+names = ["Donald Trump",
+         "Joe Biden",
+         "Conan O'brien",
+         "Ellen Degeneres",
+         "Kim Kardashian",
+         "Lebron James",
+         "Lady Gaga",
+         "Cristiano Ronaldo",
+         "Jimmy kimmel",
+         "Arnold schwarzenegger"]
+
+
+def get_tweets(path):
+    return pd.read_csv(path)
 
 
 def extract_hashtags():
@@ -10,5 +30,14 @@ def extract_tags():
     pass
 
 
-def extract_len():
-    pass
+def extract_len(tweet):
+    tweet = tweet.split(' ')
+    number_of_words = len(tweet)
+    longest_word_length = min(tweet, key = len)
+    shortest_word_length = max(tweet, key = len)
+    return tweet, number_of_words, longest_word_length, shortest_word_length
+
+
+def main():
+    for path in pathes:
+        tweets = get_tweets(path)
