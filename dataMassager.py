acef42e41ad6af62461aa5f84e08ced25fee17d1
Michael <michaelbere@gmail.com>
2019-06-06 11:06:13 +0300

    small bug fixes

--- dataMassager.py
+++ dataMassager.py
@@ -7,7 +7,7 @@ import os
 
 def read_all_files(dir):
     result = []
-    for filename in tqdm(os.listdir(dir)):
+    for filename in tqdm(os.listdir(dir), desc="reading"):
         if filename.endswith(".csv") and "tweets" in filename:
             result.append(pd.read_csv(os.path.join(dir, filename)))
     return result
@@ -16,18 +16,18 @@ def read_all_files(dir):
 def split_train_test(*args, ratio=0.75):
     train = None
     test = None
-    for data in args:
+    for data in tqdm(args, desc="spliting"):
         # data = data.reindex(np.random.permutation(data.index))
         new_train = data.sample(frac=ratio)
         new_test = data.drop(new_train.index)
-        if not train:
+        if train is None:
             train = new_train
         else:
-            train = pd.concat(train, new_train)
-        if not test:
+            train = pd.concat([train, new_train])
+        if test is None:
             test = new_test
         else:
-            train = pd.concat(test, new_test)
+            test = pd.concat([test, new_test])
     return train, test
 
 
@@ -38,4 +38,7 @@ if __name__ == '__main__':
     data_array = read_all_files(data_file_dir)
 
     train_data, test_data = split_train_test(*data_array, ratio=0.75)
+
+    for i in range(5):
+        pass
 
